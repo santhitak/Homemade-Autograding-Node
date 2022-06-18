778f6f1a65420519813bfe7e1328788bc92b6565
santhitak <santhita.sk@gmail.com>
2022-06-18 14:02:53 +0700

    check

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-function hello() {
+function hello(arr) {
   var sum = 0;
-  var numbers = [3, 4, 3, 1, 5, 32, 2];
-  numbers.forEach((value, index, array) => {
+  arr.forEach((value, index, array) => {
     if (index % 2 === 0) {
       sum += value;
     }
--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,7 @@
 const hello = require("./hello");
 
+var numbers = [3, 4, 3, 1, 5, 32, 2];
+
 test("outputs", () => {
-  expect(hello()).toBe(hello());
+  expect(hello(numbers)).toBe(13);
 });
