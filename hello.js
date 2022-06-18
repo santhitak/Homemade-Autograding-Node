79ea2dcc32045417a480c8816e4f6d0a9f19ae23
santhitak <santhita.sk@gmail.com>
2022-06-18 13:46:55 +0700

    this is it

--- hello.js
+++ hello.js
@@ -1,5 +1,12 @@
 function hello() {
-  return "Hello!";
+  var sum = 0;
+  var numbers = [3, 4, 3, 1, 5, 32, 2];
+  numbers.forEach((value, index, array) => {
+    if (index % 2 === 0) {
+      sum += value;
+    }
+  });
+  return sum;
 }
 
 module.exports = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,5 @@
-const hello = require('./hello');
+const hello = require("./hello");
 
-test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello world!");
+test("outputs the correct string", () => {
+  expect(hello()).toBe(sum);
 });
